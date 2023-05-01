95b02a1361d8f931d128d281faa2e6d78a65a176
HeerAnaghan <heeranaghan@gmail.com>
2023-05-01 17:19:54 +0530

    Ch2 Datatypes and variables

---
+++ Ch2/HAC3-2-2.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#define pi 3.14
+main()
+	{
+	  float r;
+	  clrscr();
+	  printf("Enter the value of r:");
+	  scanf("%f",&r);
+	  printf("area of circle is:%.2f",pi*r*r);
+
+
+		getch();
+	}
+
+
---
+++ Ch2/HAC3-2-B.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+	main()
+	{   int l,b;
+		clrscr();
+		printf("enter the value of l:");
+		scanf("%d",&l);
+		printf("enter the value of b;");
+		scanf("%d",&b);
+		printf("area of rectangle is:%d",l*b);
+		getch();
+	}
