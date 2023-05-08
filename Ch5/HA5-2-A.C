f38df5c63c4cb7e3ce293f0dc4bdd8b7fbb6dfe0
HeerAnaghan <heeranaghan@gmail.com>
2023-05-08 17:05:19 +0530

    new upload

---
+++ Ch5/HA5-2-A.C
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<conio.h>
+     void main()
+	{       int a,b,c,d;
+		clrscr();
+		printf("Enter a:");
+		scanf("%d",&a);
+		printf("Enter b:");
+		scanf("%d",&c);
+		printf("Enter c:");
+		scanf("%d",&c);
+		printf("Enter d:");
+		scanf("%d",&d);
+
+		if(a>b)
+		{
+			 if(a>c)
+			 {
+				if(a>d)
+				{
+				    printf("a is big");
+				}
+				else
+				{
+				    printf("d is big");
+				}
+			 }
+			 else
+			 {
+			     if(c>d)
+			     {
+				 printf("c is big");
+			     }
+			     else
+			     {
+				 printf("d is big");
+			     }
+
+			 }
+		}
+		else
+		{
+			 if(b>c)
+			 {
+			       if(b>d)
+			       {
+				   printf("b is big");
+			       }
+			       else
+			       {
+				   printf("d is big");
+			       }
+			 }
+			 else
+			 {
+				 if(c>d)
+				 {
+				      printf("c is big");
+				 }
+				 else
+				 {
+				      printf("d is big");
+				 }
+
+
+			 }
+
+
+
+		}
+
+
+		getch();
+
+	}
